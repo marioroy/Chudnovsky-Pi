f30c8f8ee18ccb79a3cb192018f396d6198a6cb9
Mario Roy <marioeroy@gmail.com>
2020-05-12 20:06:07 -0400

    Bumped MCE::Shared to 1.870.

--- lib/MCE/Shared/Condvar.pm
+++ lib/MCE/Shared/Condvar.pm
@@ -13,7 +13,7 @@ use 5.010001;
 
 no warnings qw( threads recursion uninitialized numeric );
 
-our $VERSION = '1.869';
+our $VERSION = '1.870';
 
 use MCE::Shared::Base ();
 use MCE::Util ();
@@ -352,7 +352,7 @@ MCE::Shared::Condvar - Condvar helper class
 
 =head1 VERSION
 
-This document describes MCE::Shared::Condvar version 1.869
+This document describes MCE::Shared::Condvar version 1.870
 
 =head1 DESCRIPTION
 
--- lib/MCE/Shared/Sequence.pm
+++ lib/MCE/Shared/Sequence.pm
@@ -13,7 +13,7 @@ use 5.010001;
 
 no warnings qw( threads recursion uninitialized numeric );
 
-our $VERSION = '1.869';
+our $VERSION = '1.870';
 
 use Scalar::Util qw( looks_like_number );
 use MCE::Shared::Base ();
@@ -229,7 +229,7 @@ MCE::Shared::Sequence - Sequence helper class
 
 =head1 VERSION
 
-This document describes MCE::Shared::Sequence version 1.869
+This document describes MCE::Shared::Sequence version 1.870
 
 =head1 DESCRIPTION
 
