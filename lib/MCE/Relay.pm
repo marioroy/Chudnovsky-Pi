d7bf74e93ea690ed1d3ef9a34a71acc7a18280b4
Mario Roy <marioeroy@gmail.com>
2023-05-30 20:32:01 -0500

    Bumped MCE to 1.885, MCE::Shared to 1.881.

--- lib/MCE/Relay.pm
+++ lib/MCE/Relay.pm
@@ -11,7 +11,7 @@ use warnings;
 
 no warnings qw( threads recursion uninitialized numeric );
 
-our $VERSION = '1.883';
+our $VERSION = '1.885';
 
 ## no critic (Subroutines::ProhibitSubroutinePrototypes)
 
@@ -368,7 +368,7 @@ MCE::Relay - Extends Many-Core Engine with relay capabilities
 
 =head1 VERSION
 
-This document describes MCE::Relay version 1.883
+This document describes MCE::Relay version 1.885
 
 =head1 SYNOPSIS
 
